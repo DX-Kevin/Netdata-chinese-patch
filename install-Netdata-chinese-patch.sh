b551df2eb850ea3e148ecf1867c3f21eba2a832d
DX.Kevin <weixingdx@163.com>
2021-06-26 16:36:40 +0800

    Update install-Netdata-chinese-patch.sh

--- install-Netdata-chinese-patch.sh
+++ install-Netdata-chinese-patch.sh
@@ -12,3 +12,4 @@ mv $M $M.bk
 mv $I $I.bk
 wget  $DL$D&&wget  $DL$DI&&wget  $DL$I&&wget  $DL$M
 chown netdata:netdata $D $DI $I $M
+echo -e "\033[40;32m-------------简体中文版Netdata补丁安装完成-------------\033[0m"
