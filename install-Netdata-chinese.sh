4521b3a08f93dc560e10c9eb399b260180ad874f
DX.Kevin <weixingdx@163.com>
2021-06-26 17:20:23 +0800

    Update install-Netdata-chinese.sh

--- install-Netdata-chinese.sh
+++ install-Netdata-chinese.sh
@@ -5,9 +5,9 @@ if [ -f "/usr/bin/yum" ] && [ -d "/etc/yum.repos.d" ]; then
       PM="apt-get"
 fi
 if [ "${PM}" = "yum" ]; then
-  yum install update -y
+  yum install epel-release -y&&yum update -y
     elif [ "${PM}" = "apt-get" ]; then
-        apt-get install update -y
+        apt-get install epel-release -y&&apt-get update -y
 fi
 curl -Ss https://my-netdata.io/kickstart.sh|bash
 curl -Ss https://raw.githubusercontent.com/DX-Kevin/Netdata-chinese-patch/main/install-Netdata-chinese-patch.sh|bash
