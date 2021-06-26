862b4c366dab8b90111ed9c5ddb68717681b03b1
DX.Kevin <weixingdx@163.com>
2021-06-26 17:43:32 +0800

    Update install-Netdata-chinese-patch.sh

--- install-Netdata-chinese-patch.sh
+++ install-Netdata-chinese-patch.sh
@@ -5,10 +5,7 @@ DI="dashboard_info.js"
 I="index.html"
 M="main.js"
 C="/usr/share/netdata/web/"
-mv $C$DI $C$DI.bk
-mv $C$D $C$D.bk
-mv $C$M $C$M.bk
-mv $C$I $C$I.bk
+mv $C$DI $C$DI.bk&&mv $C$D $C$D.bk&&mv $C$M $C$M.bk&&mv $C$I $C$I.bk
 wget -P $C $DL$D&&wget -P $C $DL$DI&&wget -P $C $DL$I&&wget -P $C $DL$M
 chown netdata:netdata $C$D $C$DI $C$I $C$M
 echo -e "\033[40;32m-------------简体中文版Netdata补丁安装完成-------------\033[0m"
