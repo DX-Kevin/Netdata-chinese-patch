b32ee3a81ff6d474a4123cbaf2cbb3918119ae88
DX.Kevin <weixingdx@163.com>
2021-06-26 17:15:31 +0800

    Update install-Netdata-chinese.sh

--- install-Netdata-chinese.sh
+++ install-Netdata-chinese.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 if [ -f "/usr/bin/yum" ] && [ -d "/etc/yum.repos.d" ]; then
   PM="yum"
-elif [ -f "/usr/bin/apt-get" ] && [ -f "/usr/bin/dpkg" ]; then
-  PM="apt-get"
+    elif [ -f "/usr/bin/apt-get" ] && [ -f "/usr/bin/dpkg" ]; then
+      PM="apt-get"
 fi
 if [ "${PM}" = "yum" ]; then
   yum install update -y
-elif [ "${PM}" = "apt-get" ]; then
-  apt-get install update -y
+    elif [ "${PM}" = "apt-get" ]; then
+        apt-get install update -y
 fi
 curl -Ss https://my-netdata.io/kickstart.sh|bash
 curl -Ss https://raw.githubusercontent.com/DX-Kevin/Netdata-chinese-patch/main/install-Netdata-chinese-patch.sh|bash
