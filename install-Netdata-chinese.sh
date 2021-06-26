36f7d11e2fbaa056fc1af571389ab45218fbd5c3
DX.Kevin <weixingdx@163.com>
2021-06-26 16:55:05 +0800

    Update install-Netdata-chinese.sh

--- install-Netdata-chinese.sh
+++ install-Netdata-chinese.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
+if [ ! -f /bin/curl ];then
+  if [ "${PM}" = "yum" ]; then
+    yum install curl -y
+  elif [ "${PM}" = "apt-get" ]; then
+    apt-get install curl -y
+  fi
+fi
 curl -Ss https://my-netdata.io/kickstart.sh|bash
 curl -Ss https://raw.githubusercontent.com/DX-Kevin/Netdata-chinese-patch/main/install-Netdata-chinese-patch.sh|bash
 echo -e "\033[40;32m-------------中文版Netdata安装完成-------------\033[0m"
