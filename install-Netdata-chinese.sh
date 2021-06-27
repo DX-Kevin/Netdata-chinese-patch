c592df982763d360439427c8ef8a12c205c07125
DX.Kevin <weixingdx@163.com>
2021-06-27 11:41:57 +0800

    Update install-Netdata-chinese.sh

--- install-Netdata-chinese.sh
+++ install-Netdata-chinese.sh
@@ -554,6 +554,16 @@ DI="dashboard_info.js"
 I="index.html"
 M="main.js"
 C="/usr/share/netdata/web/"
+if [ -f "/usr/bin/yum" ] && [ -d "/etc/yum.repos.d" ]; then
+  PM="yum"
+    elif [ -f "/usr/bin/apt-get" ] && [ -f "/usr/bin/dpkg" ]; then
+      PM="apt-get"
+fi
+if [ "${PM}" = "yum" ]; then
+  yum install wget -y
+    elif [ "${PM}" = "apt-get" ]; then
+        apt-get install wget -y
+fi
 mv $C$DI $C$DI.bk&&mv $C$D $C$D.bk&&mv $C$M $C$M.bk&&mv $C$I $C$I.bk
 wget -P $C $DL$D&&wget -P $C $DL$DI&&wget -P $C $DL$I&&wget -P $C $DL$M
 chown netdata:netdata $C$D $C$DI $C$I $C$M
