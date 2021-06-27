7666b7e7037c5fde57c729a965eef76293d9eaf8
DX.Kevin <weixingdx@163.com>
2021-06-27 11:00:20 +0800

    Update install-Netdata-chinese-patch.sh

--- install-Netdata-chinese-patch.sh
+++ install-Netdata-chinese-patch.sh
@@ -16,10 +16,7 @@ if [ "${PM}" = "yum" ]; then
         apt-get install wget -y
 fi
 mv $C$DI $C$DI.bk&&mv $C$D $C$D.bk&&mv $C$M $C$M.bk&&mv $C$I $C$I.bk
-echo -e "\033[40;32m-------------1-------------\033[0m"
 wget -P $C $DL$D&&wget -P $C $DL$DI&&wget -P $C $DL$I&&wget -P $C $DL$M
-echo -e "\033[40;32m-------------2-------------\033[0m"
 chown netdata:netdata $C$D $C$DI $C$I $C$M
-echo -e "\033[40;32m-------------3-------------\033[0m"
 systemctl stop netdata&&systemctl start netdata
 echo -e "\033[40;32m-------------简体中文版Netdata补丁安装完成-------------\033[0m"
